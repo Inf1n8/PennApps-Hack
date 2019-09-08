7e672a3cece77860db9823c1bb807ad363d5765e
userv562 <userv562@users.noreply.github.com>
2019-09-07 22:10:47 -0400

    changes

--- detection.py
+++ detection.py
@@ -1,12 +1,15 @@
-import io
-import os
 from PIL import Image
+import numpy as np
+import cv2
+from google.cloud import vision
+import base64
 
-imagePath = ""
+# os.system('export GOOGLE_APPLICATION_CREDENTIALS="/home/pi/Desktop/Assistant/gcp/lat.json"')
 
 def findDistances(gun, people):
     print("===========================")
     (gXMin, gYMin, gXMax, gYMax) = gun
+
     gXCenter = (gXMax + gXMin / 2.)
     gYCenter = (gYMax + gYMin / 2.)
     print("gXCenter: ", gXCenter)
@@ -19,12 +22,12 @@ def findDistances(gun, people):
     for (i, (pXMin, pYMin, pXMax, pYMax)) in enumerate(people):
         pXCenter = (pXMax + pXMin / 2.)
         pYCenter = (pYMax + pYMin / 2.)
-        print("pXCenter: ",pXCenter)
+        print("pXCenter: ", pXCenter)
         print("pYCenter: ", pYCenter)
 
-        tmpX = (gXCenter - pXCenter)**2
-        tmpY = (gYCenter - pYCenter)**2
-        dist = (tmpX + tmpY)**0.5
+        tmpX = (gXCenter - pXCenter) ** 2
+        tmpY = (gYCenter - pYCenter) ** 2
+        dist = (tmpX + tmpY) ** 0.5
         print("dist: ", dist)
         if dist < distance:
             distance = dist
@@ -41,11 +44,11 @@ def localize_objects(path):
     Args:
     path: The path to the local file.
     """
-    from google.cloud import vision
     client = vision.ImageAnnotatorClient()
 
     with open(path, 'rb') as image_file:
-        content = image_file.read()
+     content = image_file.read()
+    # image = client.image(content=cv2.imencode('.jpg', path)[1].tostring())
     image = vision.types.Image(content=content)
 
     objects = client.object_localization(
@@ -65,7 +68,7 @@ def localize_objects(path):
 
             for (i, vertex) in enumerate(object_.bounding_poly.normalized_vertices):
                 if i == 0:
-                    xMin= vertex.x
+                    xMin = vertex.x
                     yMin = vertex.y
                 if i == 1:
                     xMax = vertex.x
@@ -82,7 +85,7 @@ def localize_objects(path):
             xMin, ymin, xMax, yMax = 0.0, 0.0, 0.0, 0.0
             for (i, vertex) in enumerate(object_.bounding_poly.normalized_vertices):
                 if i == 0:
-                    xMin= vertex.x
+                    xMin = vertex.x
                     yMin = vertex.y
                 if i == 1:
                     xMax = vertex.x
@@ -96,23 +99,49 @@ def localize_objects(path):
 
 
 
+cap = cv2.VideoCapture(0)
 
+while (1):
+    print("in0")
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    cv2.imshow("Live", frame)
+    cv2.imwrite("test.jpg", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-gun, people = localize_objects(imagePath)
-if not gun or not people:
-    pass
-else:
-    print(gun)
-    print(people)
-    id, distance, (ROIxMin, ROIyMin, ROIxMax, ROIyMax) = findDistances(gun, people)
-    print(id, distance, ROIxMin, ROIyMin, ROIxMax, ROIyMax)
+    print("in1")
+    imagePath = "test.jpg"
+    # img = cv2.imread("IMG_1944.jpeg")
+    # print(type(img))
+    # encoded = base64.b64encode(img)
+    # print(encoded)
 
-    im = Image.open(imagePath)
-    width, height = im.size
-    left = width * ROIxMin
-    top = height - (height * ROIyMax)
-    right = width * ROIxMax
-    bottom = height - (height * ROIyMin)
-    print(left, top, right, bottom)
-    im1 = im.crop((left, top, right, bottom))
-    im1.show()
+    # with open("IMG_1944.jpeg", 'rb') as image_file:
+    #     content = image_file.read()
+    #     print(content)
+    # encoded = base64.b64encode(content)
+
+    gun, people = localize_objects(imagePath)
+    if not gun or not people:
+        print("No threat detected")
+        print(gun, people)
+        pass
+    else:
+        print(gun)
+        print(people)
+        id, distance, (ROIxMin, ROIyMin, ROIxMax, ROIyMax)  = findDistances(gun, people)
+        print(id, distance, ROIxMin, ROIyMin, ROIxMax, ROIyMax)
+
+        im = Image.open(imagePath)
+        width, height = im.size
+        left = width * ROIxMin
+        top = height - (height * ROIyMax)
+        right = width * ROIxMax
+        bottom = height - (height * ROIyMin)
+        print(left, top, right, bottom)
+        im1 = im.crop((left, top, right, bottom))
+        im1.show()
+
+        cv2.imwrite("End.jpg", np.asarray(im1))
+        cv2.imshow("Output", np.asarray(im1))
